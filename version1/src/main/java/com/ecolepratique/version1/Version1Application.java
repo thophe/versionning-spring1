b057f64158c734c3d9ec8b7ea92b6b94335d24ad
Christophe Fessard <cfieformation@gmail.com>
2021-01-13 18:25:36 +0100

    Ajout du message "Modification 2"

--- version1/src/main/java/com/ecolepratique/version1/Version1Application.java
+++ version1/src/main/java/com/ecolepratique/version1/Version1Application.java
@@ -8,6 +8,7 @@ public class Version1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Version1Application.class, args);
+		System.out.println("Modification 2");
 	}
 
 }
