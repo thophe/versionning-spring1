4ce316be1ccf4b8ae0f8b4429dc99354cf0d69be
Christophe Fessard <cfieformation@gmail.com>
2021-01-13 16:31:00 +0100

    Version initiale du projet

---
+++ version1/src/test/java/com/ecolepratique/version1/Version1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecolepratique.version1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Version1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
